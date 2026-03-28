a043f2b2f39f0db06e5bea086f892858eed12215
nikkibuttowsk <nikkilimcomia@gmail.com>
2026-03-28 19:46:55 +0800

    initializing project structure

---
+++ WinFormsCollab/Form1.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace WinFormsApp
+{
+    public class Form1 : Form
+    {
+        public Form1()
+        {
+            Text = "Starter Form";
+        }
+    }
+}
