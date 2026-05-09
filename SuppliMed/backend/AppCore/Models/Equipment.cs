53bbde2e45cba89e8fce0b85f9235caf71822b6e
nikkibuttowsk <nikkilimcomia@gmail.com>
2026-05-09 22:03:37 +0800

    feat(equipment.cs): remove unused logic

--- SuppliMed/backend/AppCore/Models/Equipment.cs
+++ SuppliMed/backend/AppCore/Models/Equipment.cs
@@ -3,17 +3,6 @@ namespace AppCore.Models
     public class Equipment : MedicalSupply
     {
         public string? SerialNumber { get; set; }
-        public DateTime LastMaintenanceDate { get; set; }
-
-        public bool NeedsMaintenance(int months)
-        {
-            return (DateTime.Now - LastMaintenanceDate).TotalDays >= months * 30;
-        }
-
-        public void PerformMaintenance()
-        {
-            LastMaintenanceDate = DateTime.Now;
-        }
 
         public override string GetDetails()
         {
